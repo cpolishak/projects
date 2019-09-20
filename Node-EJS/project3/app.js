fb9715386fbf9f360b93ed82bfeb58620b3f5a31
cpolishak <chrispolishak@gmail.com>
2019-09-19 20:57:31 -0500

    updating to test heroku

--- Node-EJS/project3/app.js
+++ Node-EJS/project3/app.js
@@ -14,6 +14,6 @@ app.get("/contact", (req, res) => {
 });
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
    });
